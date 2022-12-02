e714810fc47dc5320d611408407cc8cdce77368c
MaximDeGiovanni <114994423+MaximDeGiovanni@users.noreply.github.com>
2022-12-02 20:12:46 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,2 +1,10 @@
+bool num_prim(int n, int i)
+{
 
-bool num_prim(int n, int i);
+    if (i<2)
+        return true;
+
+    if ( n % i == 0)
+        return false;
+    else
+        num_prim(n, i - 1);
