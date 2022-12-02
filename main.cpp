c902d2001a63f7cbbdc37d58b884e2b2a2c5200d
MaximDeGiovanni <114994423+MaximDeGiovanni@users.noreply.github.com>
2022-12-02 20:07:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
-
-int main(){
+#include "lib.h"
+int main(){ù
+  int n;
+    int i;
+    bool ris;
+    cin>>n;
+    i=n-1;
+    ris=num_prim(n,i);
+    if (ris==1)
+        cout<<"numero primo";
+    else
+        cout<<"numero non primo";
   return 0;
 }
